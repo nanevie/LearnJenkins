a1024bd52611a500e779792982fe23da893a587f
ParthaK8 <partha_kaushik@hotmail.com>
2020-11-08 16:36:43 -0500

    cicd nodejs project

---
+++ cicd/clone_nodeproject_run_tests.sh
@@ -0,0 +1,8 @@
+cd ${HOME}
+mkdir -p gitrepos
+cd gitrepos
+rm -rf examples
+git clone "https://github.com/nodejs/examples"
+cd ./examples/servers/express/api-with-express-and-handlebars
+npm install
+npm test
